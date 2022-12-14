2b7acebc5c8ecc923b60c6ce9ab87afb8bb23de5
nmasclans <nuria.masclans@upc.edu>
2022-12-14 16:44:22 +0100

    new bash file

---
+++ execute_in_Hybrid_train_MLP_inputScalar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for HIDDEN_LAYERS in 1 2; do
+    for NEURONS_PER_HIDDEN_LAYER in 8; do
+        echo "\n----------------------------------------------------------------------------------"
+        echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER"
+        XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda python3 train_MLP_InputScalar.py --num_hidden_layers $HIDDEN_LAYERS --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER --num_epochs 1
+    done
+done
