a8f2c57e328005cb4fda7b0a6bd800660e680de4
nmasclans <nuria.masclans@upc.edu>
2022-12-20 19:48:45 +0100

    fix bash files bugs

--- execute_in_BSC_trainer.sh
+++ execute_in_BSC_trainer.sh
@@ -13,24 +13,22 @@ module load python/3.7.4_ML
 module load tensorflow/2.5.0
 
 
-for HIDDEN_LAYERS in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+for HIDDEN_LAYERS in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
     for NEURONS_PER_HIDDEN_LAYER in 8 16 32 64 128 256; do
         for LEARNING_RATE in 0.001 0.0001; do
-        
-        echo ""
-        echo "----------------------------------------------------------------------------------"
-        echo "----------------------------------------------------------------------------------"
-        echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Learning rate: $LEARNING_RATE"
-        echo "----------------------------------------------------------------------------------"
-        echo "----------------------------------------------------------------------------------"
-        
-        python3 my_trainer.py \
-		    --num_hidden_layers $HIDDEN_LAYERS \
-		    --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
-            --learning_rate $LEARNING_RATE \
-	        --num_epochs 30 \
-		    --training_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_53900000.npz" \
-		    --validation_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_59300000.npz"
-
+            echo ""
+            echo "----------------------------------------------------------------------------------"
+            echo "----------------------------------------------------------------------------------"
+            echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Learning rate: $LEARNING_RATE"
+            echo "----------------------------------------------------------------------------------"
+            echo "----------------------------------------------------------------------------------"
+            python3 my_trainer.py \
+		        --num_hidden_layers $HIDDEN_LAYERS \
+		        --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
+                --learning_rate $LEARNING_RATE \
+	            --num_epochs 30 \
+		        --training_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_53900000.npz" \
+		        --validation_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_59300000.npz"
+        done
     done
 done
--- execute_in_Hybrid_trainer.sh
+++ execute_in_Hybrid_trainer.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+#SBATCH --output=PINNS_training_%j.out
 
-for HIDDEN_LAYERS in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+for HIDDEN_LAYERS in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
     for NEURONS_PER_HIDDEN_LAYER in 8 16 32 64 128 256; do
         for LEARNING_RATE in 0.001 0.0001; do
-        
-        echo ""
-        echo "----------------------------------------------------------------------------------"
-        echo "----------------------------------------------------------------------------------"
-        echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Learning rate: $LEARNING_RATE"
-        echo "----------------------------------------------------------------------------------"
-        echo "----------------------------------------------------------------------------------"
-        
-        XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda python3 my_trainer.py \
-		    --num_hidden_layers $HIDDEN_LAYERS \
-		    --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
-            --learning_rate $LEARNING_RATE \
-	        --num_epochs 30 \
+            echo ""
+            echo "----------------------------------------------------------------------------------"
+            echo "----------------------------------------------------------------------------------"
+            echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Learning rate: $LEARNING_RATE"
+            echo "----------------------------------------------------------------------------------"
+            echo "----------------------------------------------------------------------------------"
+            XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda python3 my_trainer.py \
+		        --num_hidden_layers $HIDDEN_LAYERS \
+		        --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
+                --learning_rate $LEARNING_RATE \
+	            --num_epochs 30
+        done
     done
 done
