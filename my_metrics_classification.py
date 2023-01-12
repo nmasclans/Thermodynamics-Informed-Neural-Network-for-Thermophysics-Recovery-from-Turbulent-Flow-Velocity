c6a3bf30939db1ac6c2fda468f9150997bc10287
nmasclans <nuria.masclans@upc.edu>
2023-01-12 13:12:27 +0100

    failed f1score metric

--- my_metrics_classification.py
+++ my_metrics_classification.py
@@ -1,4 +1,7 @@
+import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
+
 
 NUM_CLASSES = 3
 
@@ -6,7 +9,6 @@ NUM_CLASSES = 3
 def compute_metrics(y_gt, y_pred):
     compute_metric_IoU(y_gt, y_pred)
     compute_metric_accuracy(y_gt, y_pred)
-
 
 def compute_metric_IoU(y_gt, y_pred):
     per_class_iou = []
@@ -17,7 +19,7 @@ def compute_metric_IoU(y_gt, y_pred):
     m = tf.keras.metrics.MeanIoU(num_classes = NUM_CLASSES)
     m.update_state(y_gt, y_pred)
     mean_iou = m.result().numpy()
-    tf.print(f"\nMean IoU: {mean_iou:.3f}")
+    tf.print(f"\nWeighted IoU: {mean_iou:.3f}")
     tf.print(f"Per-Class IoU: \n    liquid-like: {per_class_iou[0]:.3f} \n    two-phases-like: {per_class_iou[1]:.3f} \n    gas-like: {per_class_iou[2]:.3f}\n")
 
 
@@ -26,4 +28,17 @@ def compute_metric_accuracy(y_gt, y_pred):
     m.update_state(y_gt, y_pred)
     tf.print(f"Accuracy: {m.result().numpy():.3f}\n")
 
-
+"""
+def compute_metric_F1Score(y_gt, y_pred):
+    y_gt   = y_gt.astype(np.int32)
+    y_pred = y_pred.astype(np.float32)
+    tf.print(y_gt.shape)
+    tf.print(y_pred.shape)
+    mean_F1      = tfa.metrics.F1Score(num_classes = NUM_CLASSES, average='weighted')
+    per_class_F1 = tfa.metrics.F1Score(num_classes = NUM_CLASSES, average=None)
+    tf.print(y_pred)
+    mean_F1.update_state(y_gt, y_pred)
+    per_class_F1.update_state(y_gt, y_pred)
+    tf.print(f"\nWeigthed F1 Score: {mean_F1.result().numpy():.3f}")
+    tf.print(f"Per-Class F1 Score: \n    liquid-like: {per_class_F1[0]:.3f} \n    two-phases-like: {per_class_F1[1]:.3f} \n    gas-like: {per_class_F1[2]:.3f}\n")
+"""
